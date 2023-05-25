09e4fb2b2ffb8f65b837df51e2ae410d54ff6f52
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-05-25 21:12:19 +0700

    route.rb

--- route.rb
+++ route.rb
@@ -1,17 +1,17 @@
-class Route #Маршрут
-  attr_reader  :route, :stations
+class Route
+  attr_reader :route, :stations
 
   def initialize(station_first, station_last)
     @route = [station_first, station_last]
   end
 
-  def add_station(station) #Добавление станции
-      @route.insert(-2, station)
-    end
+  def add_station(station)
+    @route.insert(-2, station)
   end
 
-  def remove_station(station) # Удаление станции
+  def remove_station(station)
     if stations != @route.first || station != route.last
       stations.delete(station)
     end
   end
+end
